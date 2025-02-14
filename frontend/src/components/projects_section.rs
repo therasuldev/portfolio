7ac88ea35de00264dcd4157421ce6bb3eaab309c
therasuldev <rasul.ramixanov@gmail.com>
2025-02-14 16:35:30 +0400

    enhance styling and layout for projects section with improved typography and animations

--- frontend/src/components/projects_section.rs
+++ frontend/src/components/projects_section.rs
@@ -8,18 +8,17 @@ pub struct Project {
     pub description: String,
     pub link: String,
 }
-
 #[derive(Properties, PartialEq)]
 pub struct ProjectsSectionProps {
     pub projects: Vec<Project>,
     pub error: Option<String>,
 }
-
 #[function_component]
 pub fn ProjectsSection(props: &ProjectsSectionProps) -> Html {
     html! {
-        <div class="max-w-6xl w-full mb-4 mx-auto">
-            <h2 class="text-2xl font-bold text-gray-800 mb-3 text-center">{"Top Projects"}</h2>
+        <div class="max-w-6xl w-full mb-4 mx-auto projects-section" style="font-family: 'Space Mono', monospace; width: 80%;">
+            <h1 class="text-2xl font-bold mb-3 text-center" style="color: #08504B;">{"Top Projects"}</h1>
+
             {
                 if let Some(error_message) = &props.error {
                     html! {
@@ -31,25 +30,41 @@ pub fn ProjectsSection(props: &ProjectsSectionProps) -> Html {
                     html! {}
                 }
             }
-            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 justify-items-center">
+            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 justify-items-center ">
                 {
                     props.projects.iter().map(|project| {
                         html! {
-                            <div key={project.id} class="bg-white rounded-lg shadow-xl hover:shadow-2xl transition-all transform hover:scale-105 flex flex-col">
+                            <div key={project.id} class="border border-gray-700 rounded-lg transform hover:scale-105 transition-all flex flex-col bg-gradient-to-br from-gray-900 via-black to-gray-800">
                                 <div class="px-6 py-4 flex-grow">
-                                    <h3 class="text-base font-bold text-gray-800 mb-2 text-center">{&project.name}</h3>
-                                    <p class="text-gray-500 text-sm mb-4 text-center"
-                                       style="display: -webkit-box; -webkit-line-clamp: 5; -webkit-box-orient: vertical; overflow: hidden; text-overflow: ellipsis;">
+                                    <h3 class="text-base font-bold text-gray-800 mb-2 text-center"
+                                        style="font-family: 'Space Mono', monospace; color: #08504B;">
+                                        {&project.name}
+                                    </h3>
+                                    <p class="text-sm mb-4 text-center"
+                                    style="display: -webkit-box; -webkit-line-clamp: 5; color:#718096;  -webkit-box-orient: vertical; overflow: hidden; text-overflow: ellipsis; font-family: 'Space Mono', monospace;">
                                         {&project.description}
                                     </p>
                                 </div>
                                 <a
                                     href={project.link.clone()}
-                                    class="inline-block px-4 py-2 text-sm text-white bg-blue-500 rounded-lg hover:bg-blue-600 transition mx-auto mb-4"
+                                    class="inline-block px-4 py-2 text-sm text-white bg-blue-500 rounded-lg hover:bg-blue-600 transition mx-auto mb-4 relative overflow-hidden shimmer"
                                     target="_blank"
                                     rel="noopener noreferrer"
+                                    style="font-family: 'Space Mono', monospace; position: relative; background: linear-gradient(90deg, #4F46E5 0%, #60A5FA 50%, #4F46E5 100%); background-size: 200% auto; animation: shimmer 2s infinite linear;"
                                 >
                                     {"View Project"}
+                                    <style>
+                                        {"
+                                        @keyframes shimmer {
+                                            0% {
+                                                background-position: 0% center;
+                                            }
+                                            100% {
+                                                background-position: 200% center;
+                                            }
+                                        }
+                                        "}
+                                    </style>
                                 </a>
                             </div>
                         }
